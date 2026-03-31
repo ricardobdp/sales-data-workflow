65d59de5e52fdde5f64480ddd344ca0088ec24a3
ricardo <ricardorbdp@gmail.com>
2026-03-31 14:32:41 -0500

    Create pruebas_calidad.sql

---
+++ sql/pruebas_calidad.sql
@@ -0,0 +1,9 @@
+-- Prueba de Integridad (Null Check)
+SELECT 'Error: Nulos encontrados' AS Status, COUNT(*) AS Cantidad_Errores
+FROM online_retail_dataset
+WHERE InvoiceNo IS NULL OR CustomerID IS NULL;
+
+-- Prueba de Lógica de Negocio (Price & Quantity Check)
+SELECT InvoiceNo, Quantity, UnitPrice
+FROM online_retail_dataset
+WHERE Quantity <= 0 OR UnitPrice <= 0;
